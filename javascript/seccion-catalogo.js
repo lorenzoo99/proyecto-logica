1e99834a45d9d96cf1045929bf53aacb6b9d41f4
felipe segura <afsegura@ucundinamarca.edu.co>
2022-05-19 13:27:21 -0500

    responsiv con menu desplegable

--- javascript/seccion-catalogo.js
+++ javascript/seccion-catalogo.js
@@ -1,9 +1,398 @@
-document.getElementById('s22+').addEventListener('click', function() {
-	var precio_s22 = 4459000;
-	var iva_s22 = (precio_s22 * 0.19);
-	var precio_total_s22 = precio_s22 + iva_s22;
+
+document.getElementById('s22+')
+.addEventListener('click', function(event) {
+	precio_s22 = 4459000;
+	iva_s22 = (precio_s22 * 0.19);
+	precio_total_s22 = precio_s22 + iva_s22;
 	alert('Subtotal:          '+ precio_s22 +'\n' + 'Iva:                     '+ iva_s22 + '\n' + 'Precio total:     ' + precio_total_s22  );
 
 	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
 
 })
+document.getElementById('s22-ultra')
+.addEventListener('click', function(event) {
+	precio_s22_ultra = 5469900;
+	iva_s22_ultra = (precio_s22_ultra * 0.19);
+	precio_total_s22_ultra = precio_s22_ultra + iva_s22_ultra;
+	alert('Subtotal:          '+ precio_s22_ultra +'\n' + 'Iva:                   '+ iva_s22_ultra + '\n' + 'Precio total:     ' + precio_total_s22_ultra );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+document.getElementById('s21')
+.addEventListener('click', function(event) {
+	precio_s21 = 2620900;
+	iva_s21 = (precio_s21 * 0.19);
+	precio_total_s21 = precio_s21 + iva_s21;
+	alert('Subtotal:          '+ precio_s21 +'\n' + 'Iva:                     '+ iva_s21 + '\n' + 'Precio total:     ' + precio_total_s21 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('note20')
+.addEventListener('click', function(event) {
+	precio_note20 = 3947000;
+	iva_note20 = (precio_note20 * 0.19);
+	precio_total_note20 = precio_note20 + iva_note20 ;
+	alert('Subtotal:          '+ precio_note20 +'\n' + 'Iva:                     '+ iva_note20  + '\n' + 'Precio total:     ' + precio_total_note20 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('A52')
+.addEventListener('click', function(event) {
+	precio_A52 = 1118500;
+	iva_noteA52 = (precio_A52 * 0.19);
+	precio_total_A52 = precio_A52 + iva_noteA52 ;
+	alert('Subtotal:          '+ precio_A52 +'\n' + 'Iva:                     '+ iva_noteA52  + '\n' + 'Precio total:     ' + precio_total_A52 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('samsung_A51')
+.addEventListener('click', function(event) {
+	precio_samsung_A51 = 1226900;
+	iva_samsung_A51 = (precio_samsung_A51 * 0.19);
+	precio_total_samsung_A51 = precio_samsung_A51 + iva_samsung_A51 ;
+	alert('Subtotal:          '+ precio_samsung_A51 +'\n' + 'Iva:                     '+ iva_samsung_A51  + '\n' + 'Precio total:     ' + precio_total_samsung_A51 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('i13')
+.addEventListener('click', function(event) {
+	precio_i13 = 5499900  ;
+	iva_i13 = (precio_i13 * 0.19);
+	precio_total_i13 = precio_i13 + iva_i13;
+	alert('Subtotal:          '+ precio_i13 +'\n' + 'Iva:                   '+ iva_i13  + '\n' + 'Precio total:     ' + precio_total_i13 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('i12')
+.addEventListener('click', function(event) {
+	precio_i12 = 4459900 ;
+	iva_i12 = (precio_i12 * 0.19);
+	precio_total_i12 = precio_i12 + iva_i12;
+	alert('Subtotal:          '+ precio_i12 +'\n' + 'Iva:                     '+ iva_i12  + '\n' + 'Precio total:     ' + precio_total_i12 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('i11')
+.addEventListener('click', function(event) {
+	precio_i11 = 2715900  ;
+	iva_i11 = (precio_i11 * 0.19);
+	precio_total_i11 = precio_i11 + iva_i11;
+	alert('Subtotal:          '+ precio_i11 +'\n' + 'Iva:                     '+ iva_i11  + '\n' + 'Precio total:     ' + precio_total_i11 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('ixs')
+.addEventListener('click', function(event) {
+	precio_ixs = 2800000;
+	iva_ixs = (precio_ixs * 0.19);
+	precio_total_ixs = precio_ixs + iva_ixs;
+	alert('Subtotal:          '+ precio_ixs +'\n' + 'Iva:                     '+ iva_ixs  + '\n' + 'Precio total:     ' + precio_total_ixs );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('P50')
+.addEventListener('click', function(event) {
+	precio_P50 = 5200000 ;
+	iva_P50 = (precio_P50 * 0.19);
+	precio_total_P50 = precio_P50 + iva_P50;
+	alert('Subtotal:          '+ precio_P50 +'\n' + 'Iva:                     '+ iva_P50  + '\n' + 'Precio total:     ' + precio_total_P50 );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('M40p')
+.addEventListener('click', function(event) {
+	precio_M40p = 4996900 ;
+	iva_M40p = (precio_M40p * 0.19);
+	precio_total_M40p = precio_M40p + iva_M40p;
+	alert('Subtotal:          '+ precio_M40p +'\n' + 'Iva:                     '+ iva_M40p  + '\n' + 'Precio total:     ' + precio_total_M40p );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('P40p')
+.addEventListener('click', function(event) {
+	precio_P40p = 5199000 ;
+	iva_P40p = (precio_P40p * 0.19);
+	precio_total_P40p = precio_P40p + iva_P40p;
+	alert('Subtotal:          '+ precio_P40p +'\n' + 'Iva:                     '+ iva_P40p  + '\n' + 'Precio total:     ' + precio_total_P40p );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('P40l')
+.addEventListener('click', function(event) {
+	precio_P40l = 3256000 ;
+	iva_P40l = (precio_P40l * 0.19);
+	precio_total_P40l = precio_P40l + iva_P40l;
+	alert('Subtotal:          '+ precio_P40l +'\n' + 'Iva:                     '+ iva_P40l  + '\n' + 'Precio total:     ' + precio_total_P40l );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+
+document.getElementById('Y9S')
+.addEventListener('click', function(event) {
+	precio_Y9S = 1099000 ;
+	iva_Y9S = (precio_Y9S * 0.19);
+	precio_total_Y9S = precio_Y9S + iva_Y9S;
+	alert('Subtotal:          '+ precio_Y9S +'\n' + 'Iva:                     '+ iva_Y9S  + '\n' + 'Precio total:     ' + precio_total_Y9S );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+document.getElementById('P30l')
+.addEventListener('click', function(event) {
+	precio_P30l = 1200000 ;
+	iva_P30l = (precio_P30l * 0.19);
+	precio_total_P30l = precio_P30l + iva_P30l;
+	alert('Subtotal:          '+ precio_P30l +'\n' + 'Iva:                     '+ iva_P30l  + '\n' + 'Precio total:     ' + precio_total_P30l );
+
+	
+	//console.log( precio_s22, iva_s22, precio_total_s22);
+	//event.preventDefault();
+
+})
+
+/*
+
+(function(){
+	const botonCarrito = document.getElementById('boton-carrito');
+	const slide = document.getElementById('slide');
+
+	botonCarrito.addEventListener('click', function(){
+		slide.classList.toggle('show-slide');
+	});
+
+})();
+
+
+
+
+
+// añadir celulares al carrito
+class producto {
+	constructor(nombre, precio){
+		this.nombre = nombre;
+		this.precio = precio;
+
+	};
+
+
+};
+class UI {
+	addProduct(){
+
+	};
+	deleteProduct(){
+
+	};
+
+
+
+
+};
+
+document.getElementById('s22+')
+.addEventListener('click', function(){
+	const nombre = document.getElementById('s22+_nombre').content;
+	const precio = document.getElementById('s22_precio').content;
+
+	console.log(nombre, precio);
+	alert('Ha agregado el artículo al carrito');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(function(){
+
+	const boton_compra = document.querySelectorAll(".boton-comprar");
+
+	boton_compra.forEach(function(boton) {
+		boton.addEventListener('click', function(event){
+			console.log(event.target);
+			alert('Ha agregado el artículo al carrito');
+
+		});
+	});
+});
+*/
+
+/*document.getElementsByClassName('boton-comprar').addEventListener('click', function(){
+	alert('Ha agregado el artículo al carrito');
+
+
+})();
+*/
+
+
+
+	
+/*function verTotales(){
+    let nombre = event.target.parentElement.previousElementSibling.textContent; -< precio
+	const total = [];
+	const articulos = console.log(event.target.parentElement.previousElementSibling.firstChild);
+
+	articulos.forEach(function(articulo){
+		total.push(parseFloat(articulo.textContent));
+	});
+	//console.log(total);
+	const dinerototal = total.reduce(function(total, articulo){
+		total += articulo;
+		return total;
+	},0)
+	console.log(dinerototal)*/
+
+
+
+//if (event.target.parentElement.classList.contains('boton-comprar')){
+
+
+
+
+
+
+
+
+
+
+
+
+			/*
+
+		   // let posicion = main.indexof('boton-comprar') +3 ;
+
+		    //console.log(posicion);
+		    //const articulo = {};
+
+		   const nombre = event.target.parentElement.parentNode.previousElementSibling.previousElementSibling.textContent;
+           const nombreFinal = nombre.trim();
+          // articulo.nombre = nombreFinal;
+
+           const precio = event.target.parentElement.previousElementSibling.textContent;
+           
+           const precioFinal = precio.trim().slice(1);
+
+          // articulo.precio = precioFinal;
+           const info_articulo_encarro = new articulo(nombreFinal, precioFinal);
+
+           const ui = new UI();
+           ui.articuloEncarro(info_articulo_encarro);
+         
+
+		   /*const cartArticulo = document.createElement('div');
+		   cartArticulo.classList.add('portada-articulo__nombre-celular');
+		   	/*,
+		   	'd-grid',
+		   	'grid-template-columns-auto-auto',
+		   	'justify-content-between',
+		   	'padding-2vh-1vw',
+		   	'align-items-center'
+		   	);
+          
+           cartArticulo.innerHTML = ` 
+
+		    
+		        <div class="portada-articulo__nombre-celular">
+				    ${articulo.nombre}
+			    </div>
+        		<div>
+        			${articulo.precio}
+        		</div>
+        	</div>
+        	`;
+
+        	//seleccionar cart
+        	const slide = document.getElementById('slide');
+        	const total = document.querySelector('.precio.');
+
+        	slide.insertBefore(cartArticulo, total);
+        	alert('Ha agregado el artículo al carrito');/*
+
+
+
+
+
+
+/*
+	articuloEncarro(info_articulo_encarro){
+		const listaProducto = document.getElementById('slide');
+		const elemento = document.createElement('div');
+		elemento.innerHTML = ` 
+		    <div class=" tarjeta text-center mb-4">
+		       <div class ='cuerpo-tarjeta'>
+		       <strong>info_articulo_encarro nombre</strong>: ${nombreFinal}
+		       <strong>info_articulo_encarro precio</strong>: ${precioFinal}
+
+		       </div>
+		    </div>
+
+		`;
+		listaProducto.appendChild(elemento);
+	}
+*/
